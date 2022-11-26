184170e83e63d2f229ea4474af5918e25c980d30
rrigato <ryanrigato@gmail.com>
2022-11-26 16:21:23 -0600

    app deployment setup

---
+++ scripts/app_deployment.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+#exits program immediately if a command is not sucessful
+set -e
+
+export PROJECT_NAME="rewatch"
+
+source avenv/bin/activate
+
+secret_scan_results=$(detect-secrets scan | \
+python3 -c "import sys, json; print(json.load(sys.stdin)['results'])" )
+
+# static scan for security credentials that terminates if any secrets are found
+if [ "${secret_scan_results}" != "{}" ]; then
+    echo "detect-secrets scan failed"
+    exit 125
+fi
+
+python -m unittest
+
+deactivate
+
+git push origin dev
+
+echo "----------------------"
+echo "deployment successful"

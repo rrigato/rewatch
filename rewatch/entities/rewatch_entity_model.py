1e3cd4d171164a4bcf6778d4a71223e227e16313
rrigato <ryanrigato@gmail.com>
2022-11-27 12:33:12 -0600

    add the post_date attribute

---
+++ rewatch/entities/rewatch_entity_model.py
@@ -0,0 +1,53 @@
+from copy import deepcopy
+from datetime import date
+from typing import Optional
+
+
+class RewatchPost():
+    """business rules representing a rewatch thread"""
+    def __init__(self):
+        """Initialize all attributes to None"""
+        self.post_date = None
+        self.post_message = None
+        self.post_title = None
+
+    @property
+    def post_date(self) -> Optional[date]:
+        return(self._post_date)
+
+    @post_date.setter
+    def post_date(self, post_date: Optional[date]):
+        if type(post_date) not in (date, type(None)):
+            raise TypeError(
+                "TelevisionRating - post_date datatype " +
+                "must be a date or None")
+        self._post_date = post_date
+
+    @property
+    def post_message(self) -> Optional[str]:
+        return(self._post_message)
+
+    @post_message.setter
+    def post_message(self, post_message: Optional[str]):
+        if type(post_message) not in (
+            str, type(None)):
+            raise TypeError(
+                "RewatchPost - post_message datatype " +
+                "must be a str or None"
+            )
+        self._post_message = post_message
+
+    @property
+    def post_title(self) -> Optional[str]:
+        return(self._post_title)
+
+    @post_title.setter
+    def post_title(self, post_title: Optional[str]):
+        if type(post_title) not in (
+            str, type(None)):
+            raise TypeError(
+                "RewatchPost - post_title datatype " +
+                "must be a str or None"
+            )
+        self._post_title = post_title
+
--- tests/test_entities/test_rewatch_entity_model.py
+++ tests/test_entities/test_rewatch_entity_model.py
@@ -1,9 +1,9 @@
 import unittest
 
 class TestRewatchEntityModel(unittest.TestCase):
-
-    def test_television_rating_bad_input_str_attributes(self):
-        """TelevisionRating str attributes passed invalid input raises TypeError"""
+    
+    def test_rewatch_post_bad_input(self):
+        """invalid datatypes for entity raise TypeError"""
         from rewatch.entities.rewatch_entity_model import RewatchPost
 
         mock_invalid_types = [
@@ -29,5 +29,7 @@ class TestRewatchEntityModel(unittest.TestCase):
                 for object_property in object_properties:
                     with self.assertRaises(TypeError):
                         setattr(
-                            mock_entity, mock_invalid_type
+                            mock_entity, 
+                            mock_invalid_type,
+                            mock_invalid_type
                         )

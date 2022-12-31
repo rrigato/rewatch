1b9f993e11c1797aab3565f137d98943109f0d12
rrigato <ryanrigato@gmail.com>
2022-12-31 12:45:50 -0800

    populating entry

--- rewatch/entry/rewatch_entry.py
+++ rewatch/entry/rewatch_entry.py
@@ -3,13 +3,46 @@
 import logging
 from typing import Optional
 
+from rewatch.repo.rewatch_backend import (load_message_board_posts,
+                                          load_secret_config, submit_reddit_post)
+
 
 def create_reddit_post() -> Optional[str]:
     """creates a reddit post if matching day is found
     otherwise returns str of error
     """
-    logging.info(f"create_reddit_post - invocation begin")
     
-    logging.info(f"create_reddit_post - invocation end")
+    secret_config = load_secret_config()
+
+
+    if secret_config is None:
+        logging.info(f"create_reddit_post - short circuit credentials")
+        return("Error in pre-post setup")
+
+    logging.info(f"create_reddit_post - loaded secrets")
+
+    posts_for_today = load_message_board_posts()
+
+    if posts_for_today is None:
+        logging.info(f"create_reddit_post - short circuit post error")
+        return("Unable to load what to post today")
+    
+    if len(posts_for_today) == 0:
+        logging.info(f"create_reddit_post - short circuit no posts")
+        return("No posts for today")
+
+    logging.info(f"create_reddit_post - len posts_for_today" +
+    f"{posts_for_today}")
+
+    post_successful = submit_reddit_post(
+        posts_for_today[0], secret_config
+    )
+
+    if post_successful is not None:
+        logging.info(f"create_reddit_post - short circuit post creation")
+        return("Error creating reddit post")
+
+    logging.info(f"create_reddit_post - happy path")
+    
     return(None)
 
--- tests/test_entry/test_rewatch_entry.py
+++ tests/test_entry/test_rewatch_entry.py
@@ -6,9 +6,30 @@ from unittest.mock import MagicMock, patch
 
 class TestRewatchEntry(unittest.TestCase):
     
-    @patch("boto3.resource")
+    @patch("rewatch.entry.rewatch_entry.submit_reddit_post")
+    @patch("rewatch.entry.rewatch_entry.load_message_board_posts")
+    @patch("rewatch.entry.rewatch_entry.load_secret_config")
     def test_create_reddit_post(self,
-        boto3_resource_mock: MagicMock):
+        load_secret_config_mock: MagicMock,
+        load_message_board_posts_mock: MagicMock,
+        submit_reddit_post_mock: MagicMock
+        ):
         """Happy path for reddit post creation"""
+        from fixtures.rewatch_fixtures import (mock_message_board_posts,
+                                               mock_secret_config)
         from rewatch.entry.rewatch_entry import create_reddit_post
+
+
+        load_secret_config_mock.return_value = mock_secret_config()
+        load_message_board_posts_mock.return_value = (
+            mock_message_board_posts(1)
+        )
+        submit_reddit_post_mock.return_value = None
+
+        post_creation_error = create_reddit_post()
+
+
+        self.assertIsNone(post_creation_error)
+        
+        submit_reddit_post_mock.assert_called_once()
 

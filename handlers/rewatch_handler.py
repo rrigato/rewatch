8b4c2a3f3fe2c0522758d87f06940fe1fa5dd70a
rrigato <ryanrigato@gmail.com>
2022-12-31 16:35:11 -0800

    Lambda handler working e2e

--- handlers/rewatch_handler.py
+++ handlers/rewatch_handler.py
@@ -1,7 +1,10 @@
 
 
+import json
 import logging
 from typing import Dict, Optional
+
+from rewatch.entry.rewatch_entry import create_reddit_post
 
 
 
@@ -14,7 +17,32 @@ def rewatch_external(lambda_event: Dict, lambda_context) -> None:
         a reddit post
     """
     logging.info(f"rewatch_external - invocation begin")
+    logging.info(json.dumps(lambda_event, indent=4))
+
+    rewatch_post_error = create_reddit_post()
+
+    if rewatch_post_error is not None:
+        logging.info(rewatch_post_error)
+        logging.info(
+            f"rewatch_external - short circuiting {rewatch_post_error}"
+        )
+        raise RuntimeError("Unexpected error from architecture")
+
+    logging.info(f"rewatch_external - invocation successful")
+
     
-    logging.info(f"rewatch_external - invocation end")
-    return(None)
+
+
+
+
+if __name__ == "__main__":
+    from time import strftime
+    import logging
+    import os
+    os.environ["AWS_REGION"] = "us-east-1"
+    logging.basicConfig(
+        format="%(levelname)s | %(asctime)s.%(msecs)03d" + strftime("%z") + " | %(message)s",
+        datefmt="%Y-%m-%dT%H:%M:%S", level=logging.INFO
+    )
+    logging.info(rewatch_external({"not": "used"}, None))
 
--- tests/test_handlers/test_rewatch_handler.py
+++ tests/test_handlers/test_rewatch_handler.py
@@ -6,20 +6,23 @@ from unittest.mock import MagicMock, patch
 
 class TestRewatchHandler(unittest.TestCase):
     
-    @patch("boto3.resource")
+    @patch("handlers.rewatch_handler.create_reddit_post")
     def test_rewatch_external(self,
-        boto3_resource_mock: MagicMock):
-        """Happy path for reddit post creation"""
+        create_reddit_post_mock: MagicMock):
+        """Unexpected error raises RuntimeError"""
         from handlers.rewatch_handler import rewatch_external
 
         '''Neither lambda event data 
         or context are used by interface'''
         mock_lambda_event = {}
         mock_lambda_context = None
+        mock_error_message = "architecture layer propagated error"
 
-        self.assertIsNone(
+        create_reddit_post_mock.return_value = mock_error_message
+
+        with self.assertRaises(RuntimeError):
             rewatch_external(
                 mock_lambda_event,
                 mock_lambda_context
             )
-        )
+        

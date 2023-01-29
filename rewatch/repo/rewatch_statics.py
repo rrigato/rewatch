f43c75586abbdc2fd8534c8287578ea7528a3258
rrigato <ryanrigato@gmail.com>
2023-01-29 11:12:14 -0800

    formatting response

--- rewatch/repo/rewatch_statics.py
+++ rewatch/repo/rewatch_statics.py
@@ -1,17 +1,15 @@
 post_body_markup = """
 [Cyborg 009 2003 Toonami Intro](https://www.youtube.com/watch?v=MzOoG5YaXcU)
-\\n\\n
-\
-\r\\n
+\n
 ***Today's Episodes are:***
-  \\n
+\n
 {post_body}
-\\n
-\\n
+\n
 ***[LATER](https://www.youtube.com/watch?v=POqUUxA6z5U)***
-\\n
+\n
 ---
-Please make sure to mark spoilers for the show and future chapters accordingly.\n\n
+Please make sure to mark spoilers for the show and future chapters accordingly.
+\n
 
 ```ex: [Ep. 1 Spoiler](#s \"spoiler-text\")```  
 

8b57dc77a729d837bd903b85abdb5f1ed79eb7e5
rrigato <ryanrigato@gmail.com>
2023-02-11 09:37:20 -0800

    Update fixture to prevent fixture drift

--- fixtures/rewatch_fixtures.py
+++ fixtures/rewatch_fixtures.py
@@ -7,6 +7,29 @@ from typing import Dict
 from rewatch.entities.rewatch_entity_model import MessageBoardPost, SecretConfig
 
 
+
+
+def _validate_drift(message_board_post: MessageBoardPost) -> None:
+    """Ensurese that fixture stays in sync with entity
+    by making sure every attribute is populated
+    """
+    
+    
+    object_properties = [
+        attr_name for attr_name in dir(MessageBoardPost)
+        if not attr_name.startswith("_")
+    ]
+    
+    for object_property in object_properties:
+        assert getattr(
+            message_board_post, object_property) is not None,(
+                f"\n_validate_drift - {object_property} " +
+                "None for fixture"
+            ) 
+    return(None)
+
+
+
 def mock_message_board_posts(
         number_of_entities: int
     ) -> list[MessageBoardPost]:
@@ -20,6 +43,10 @@ def mock_message_board_posts(
         mock_entity.post_date = date.fromisoformat("2014-01-04")
         mock_entity.post_message = f"mock post_message {entity_num}"
         mock_entity.post_title = f"mock post_title {entity_num}"
+        mock_entity.show_name = f"mock show_name {entity_num}"
+        mock_entity.subreddit = f"mock subreddit {entity_num}"
+
+        _validate_drift(mock_entity)
 
         mock_entities_list.append(mock_entity)
         

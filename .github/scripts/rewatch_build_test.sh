617a33771e2d6d9a6e0750755b69b8de5048b57e
rrigato <ryanrigato@gmail.com>
2024-01-28 14:49:19 -0600

    create virtual environment and correct execution permissions

--- .github/scripts/rewatch_build_test.sh
+++ .github/scripts/rewatch_build_test.sh
@@ -2,4 +2,19 @@
 
 set -e
 
+python -m venv avenv
+
+source avenv/bin/activate
+pip install -r requirements/requirements-dev.txt
+
+secret_scan_results=$(detect-secrets scan | \
+python3 -c "import sys, json; print(json.load(sys.stdin)['results'])" )
+
+# static scan for security credentials that terminates if any secrets are found
+if [ "${secret_scan_results}" != "{}" ]; then
+    echo "detect-secrets scan failed"
+    exit 125
+fi
+
+
 python -m unittest
